c6070ac4adf90f5682aff325006eda8f16d517b7
muhammadshiva <shivamuhammad07@gmail.com>
2021-10-20 15:58:20 +0700

    init project

---
+++ app/src/main/java/com/nava/rentalmobil/activity/MainActivity.java
@@ -0,0 +1,121 @@
+package com.nava.rentalmobil.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nava.rentalmobil.R;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvToday;
+    String hariIni;
+    Animation animTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button informasi = findViewById(R.id.btn_info_mobil);
+        Button sewa = findViewById(R.id.btn_sewa);
+
+        Button tentang = findViewById(R.id.btn_tentang);
+
+        informasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, DaftarMobilActivity.class);
+                startActivity(i);
+            }
+        });
+
+        sewa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent p = new Intent(MainActivity.this, PenyewaActivity.class);
+                startActivity(p);
+            }
+        });
+
+
+        tentang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent t = new Intent(MainActivity.this, TentangActivity.class);
+                startActivity(t);
+            }
+        });
+
+        tvToday = findViewById(R.id.tvDate);
+        animTv = AnimationUtils.loadAnimation(this, R.anim.anim_tv);
+
+        Date dateNow = Calendar.getInstance().getTime();
+        hariIni = (String) DateFormat.format("EEEE", dateNow);
+        if (hariIni.equalsIgnoreCase("sunday")) {
+            hariIni = "Minggu";
+        } else if (hariIni.equalsIgnoreCase("monday")) {
+            hariIni = "Senin";
+        } else if (hariIni.equalsIgnoreCase("tuesday")) {
+            hariIni = "Selasa";
+        } else if (hariIni.equalsIgnoreCase("wednesday")) {
+            hariIni = "Rabu";
+        } else if (hariIni.equalsIgnoreCase("thursday")) {
+            hariIni = "Kamis";
+        } else if (hariIni.equalsIgnoreCase("friday")) {
+            hariIni = "Jumat";
+        } else if (hariIni.equalsIgnoreCase("saturday")) {
+            hariIni = "Sabtu";
+        }
+
+        getToday();
+    }
+
+    private void getToday() {
+        Date date = Calendar.getInstance().getTime();
+        String tanggal = (String) DateFormat.format("d", date);
+        String monthNumber = (String) DateFormat.format("M", date);
+        String year = (String) DateFormat.format("yyyy", date);
+
+        int month = Integer.parseInt(monthNumber);
+        String bulan = null;
+        if (month == 1) {
+            bulan = "Januari";
+        } else if (month == 2) {
+            bulan = "Februari";
+        } else if (month == 3) {
+            bulan = "Maret";
+        } else if (month == 4) {
+            bulan = "April";
+        } else if (month == 5) {
+            bulan = "Mei";
+        } else if (month == 6) {
+            bulan = "Juni";
+        } else if (month == 7) {
+            bulan = "Juli";
+        } else if (month == 8) {
+            bulan = "Agustus";
+        } else if (month == 9) {
+            bulan = "September";
+        } else if (month == 10) {
+            bulan = "Oktober";
+        } else if (month == 11) {
+            bulan = "November";
+        } else if (month == 12) {
+            bulan = "Desember";
+        }
+        String formatFix = hariIni + ", " + tanggal + " " + bulan + " " + year;
+        tvToday.setText(formatFix);
+    }
+
+}
---
+++ app/src/main/java/com/nava/rentalmobil/activity/TentangActivity.java
@@ -0,0 +1,14 @@
+package com.nava.rentalmobil.activity;
+import androidx.appcompat.app.AppCompatActivity;
+import com.nava.rentalmobil.R;
+import android.os.Bundle;
+
+
+public class TentangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+    }
+}
